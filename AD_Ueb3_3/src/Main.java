9eba10ed0ad81259898c5b897e84991548d03987
linusschlepp <82322027+linusschlepp@users.noreply.github.com>
2021-04-30 14:11:26 +0200

    Add files via upload

--- AD_Ueb3_3/src/Main.java
+++ AD_Ueb3_3/src/Main.java
@@ -7,23 +7,23 @@ public class Main {
 		int[] b = { -5, 13, -32, 7, -3, 17, 23, 12, -35, 19 };
 		System.out.println("Insertionsort: ");
 		long timestampInsertion = System.currentTimeMillis();
-		Insertionsort insertionsort = new Insertionsort(b);
+		InsertionSort insertionsort = new InsertionSort(b);
 		System.out.println(System.currentTimeMillis()-timestampInsertion);
 		int[] a = { -5, 13, -32, 7, -3, 17, 23, 12, -35, 19 };
 		System.out.println("Bubblesort: ");
 		long timestampBubble = System.currentTimeMillis();
-		Bubblesort bubblesort = new Bubblesort(a);
+		BubbleSort bubblesort = new BubbleSort(a);
 		System.out.println(System.currentTimeMillis()-timestampBubble);
 
 		int[] c = { -5, 13, -32, 7, -3, 17, 23, 12, -35, 19 };
 		System.out.println("Selectionsort: ");
 		long timestampSelection = System.currentTimeMillis();
-		Selectionsort selectionsort = new Selectionsort(c);
+		SelectionSort selectionsort = new SelectionSort(c);
 		System.out.println(System.currentTimeMillis()-timestampSelection);
 		
 		int[] d = { -5, 13, -32, 7, -3, 17, 23, 12, -35, 19 };
 		System.out.println("Qicksort");
-		Quicksort quicksort = new Quicksort(d);
+		QuickSort quicksort = new QuickSort(d);
 		
 	}
 
