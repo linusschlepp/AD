b62e9b092dd734cdee14140f1154e60aee670365
linusschlepp <82322027+linusschlepp@users.noreply.github.com>
2021-04-30 14:09:19 +0200

    Add files via upload

---
+++ AD_Ueb4_1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String... args) {
+		
+		int[] a = {-5, 13, -32, 7, -3, 17, 23, 12, -35, 19};
+		int[] b = {-5, 13, -32, 7, -3, 17, 23, 12, -35, 19};
+		
+		
+		System.out.println();
+		HeapSort.heapSort(b, 0, b.length-1);
+		
+
+	}
+
+}
